46f3922e5b8ffad4840dbac11bfec0eb788c71e8
Jiajun Yin <2443935y@student.gla.ac.uk>
2020-12-28 17:14:23 +0800

    change to Chinese

--- InitThoughts.py
+++ InitThoughts.py
@@ -7,9 +7,14 @@ django.setup()
 from thoughtReverse.models import Thought
 
 def Init():
-    thoughts = {'You are not good enough.': 'You are always good enough.',
-                'They were laughing at me.': 'They forgot the whole thing the next day.',
-                'I am nervous.': 'I care about it.',}
+    # thoughts = {'You are not good enough.': 'You are always good enough.',
+    #             'They were laughing at me.': 'They forgot the whole thing the next day.',
+    #             'I am nervous.': 'I care about it.',}
+
+    thoughts = {'我不够优秀。': '我已经比许多人都优秀了。',
+                '他们在笑我。': '他们第二天就全忘了。',
+                '我很紧张。': '我在意这件事，这很好。',
+                '我不知道生命的意义。': '我迟早会知道生命的意义。',}
 
     negatives = list(thoughts.keys())
     positives = list(thoughts.values())
